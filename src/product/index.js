4dae901f494148726bb12c080173be7dac04b6f9
YounSoyoung <grace902853@gmail.com>
2023-05-08 00:07:06 +0900

    id별 상품 정보 불러오기

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,23 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage(){
     // product의 id를 얻기 위해 useParams를 이용한다
     const {id} = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(function(){
+        axios.get(`https://e3e44655-948f-4c30-bd23-ab3bc78126e6.mock.pstmn.io/products/${id}`).then(
+        function(result){
+            setProduct(result.data);
+        }
+        ).catch(function(error){
+            console.error(error);
+        });
+    }, []);
+
+    console.log(product);
     return <h1>상품 상세 페이지 {id} 상품</h1>
 }
 
