7fd1d5cf9fcc657227994930056b55ea644cbdf9
YounSoyoung <grace902853@gmail.com>
2023-05-13 22:36:32 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = 'http://localhost:8080';
+//우리 컴퓨터에서 실행할 때는 process.env.NODE_ENV에 development가 들어간다.
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://grab-market-server-deploy.fly.dev' : 'http://localhost:8080';
